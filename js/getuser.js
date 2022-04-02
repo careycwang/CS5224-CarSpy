2f097c33cc40efec7c3a5db2815e75d1ac695e37
archiewang0716 <archiewang0716@foxmail.com>
2022-04-02 21:45:56 +0800

    Add files

---
+++ js/getuser.js
@@ -0,0 +1,7 @@
+import { Auth } from 'aws-amplify';
+
+const { attributes } = await Auth.currentAuthenticatedUser({
+	bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+}).then(user => console.log(user))
+.catch(err => console.log(err));
+alert(attributes);
