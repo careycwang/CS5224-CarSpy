9ef80d380a19d53c929cb1cf61c206d036b7e069
A0236053M <e0732603@u.nus.edu>
2022-04-05 15:57:51 +0800

    add listingDetail.html and other updates

--- js/test2auth.js
+++
@@ -1,6 +0,0 @@
-import { Auth } from 'aws-amplify';
-
-Auth.currentAuthenticatedUser({
-    bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-}).then(user => console.log(user))
-.catch(err => console.log(err));
